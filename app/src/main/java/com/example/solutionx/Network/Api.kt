470094c36c46573fc9aab8f0b5f5b7460ea1e705
hamzariffic <hamza.mattea@gmail.com>
2024-02-03 00:29:42 +0300

    bugs galore

--- app/src/main/java/com/example/solutionx/Network/Api.kt
+++ app/src/main/java/com/example/solutionx/Network/Api.kt
@@ -5,7 +5,7 @@ import com.example.solutionx.UserData.User
 class Api {
     suspend fun getRemoteUserData(userId: String): User {
         // Send a network request to fetch user data from the backend
-        val response = Http.get("$API_URL/users/$userId")
-        return JSON.parse(User.serializer(), response.body())
+//        val response = Http.get("$API_URL/users/$userId")
+//        return JSON.parse(User.serializer(), response.body())
     }
 }
