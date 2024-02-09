b4ed3f0f0d7e32998dcbeae5cbbdbc03a64a7509
hamzariffic <hamza.mattea@gmail.com>
2024-02-09 23:19:01 +0300

    Rearranging varriables

--- app/src/main/java/com/example/solutionx/APIService/AirQualityClient.kt
+++ app/src/main/java/com/example/solutionx/APIService/AirQualityClient.kt
@@ -7,8 +7,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object AirQualityClient {
     private const val BASE_URL = "https://airquality.googleapis.com/v1/"
-//    currentConditions:lookup?
 
+//    currentConditions:lookup?
     private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
