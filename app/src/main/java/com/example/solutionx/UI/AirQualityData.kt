26b13df393abbc2504b55a78377d13ed2c6faeac
hamzariffic <hamza.mattea@gmail.com>
2024-02-13 19:14:32 +0300

    Added air quality data together with its data class. The history interface is still here as well

--- app/src/main/java/com/example/solutionx/UI/AirQualityData.kt
+++ app/src/main/java/com/example/solutionx/UI/AirQualityData.kt
@@ -6,6 +6,7 @@ import com.example.solutionx.APIService.AirQualityResponse
 //This is the screen where users will be viewing their air quality data based on the locations they dynamically choose
 //More implementations need to be done especially on longitude and latitude bugs I parsed inside the CurrentConditions endpoint
 class AirQualityData(var airQualityResponse: String, var airQualityIndex: Int) {
+
     // Example usage in AirQualityScreen
     val airQualityResponse = AirQualityClient.airQualityApiService.CurrentConditions(latitude, longitude)
     suspend fun getCurrentAirQuality(latitude: Double, longitude: Double): AirQualityResponse {
