2e8914eb23412b69bb27ec831154ad596fa8d131
hamzariffic <hamza.mattea@gmail.com>
2024-02-13 19:12:43 +0300

    I found out the method requires POST and data classes to be integrated. The modification is ongoing

--- app/src/main/java/com/example/solutionx/APIService/AirQualityApiService.kt
+++ app/src/main/java/com/example/solutionx/APIService/AirQualityApiService.kt
@@ -1,31 +1,73 @@
 package com.example.solutionx.APIService
 
-import retrofit2.http.GET
+import com.google.type.LatLng
+import retrofit2.http.Body
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface AirQualityApiService {
-
 //    The current endpoint defines the location of the individual to provide the air quality data
-    @GET("currentConditions:lookup?")
+    @POST("currentConditions:lookup?")
     suspend fun getCurrentConditions(
-        @Query("lat") latitude: Double,
-        @Query("lon") longitude: Double,
-        // Add other necessary parameters
-    ): AirQualityResponse
-
+        @Body airQualityRequest: AirQualityRequest
+        ): AirQualityResponse
     fun CurrentConditions(latitude: Any, longitude: Any): Any
 }
 
-//This is a data class. Should be moved elsewhere
+
+interface HistoryEndpoint {
+    @POST("history:lookup")
+    suspend fun getHourlyHistory(
+        @Body request: HistoryLookupRequest
+    ): HistoryLookupResponse
+}
+
+
+//This is a data class for Air quality Data. Should be moved elsewhere
+data class AirQualityRequest(
+    val location: LatLng,
+    val extraComputations: List<ExtraComputation>,
+    val uaqiColorPalette: ColorPalette,
+    val customLocalAqis: List<CustomLocalAqi>,
+    val universalAqi: Boolean,
+    val languageCode: String
+)
+
 data class AirQualityResponse(
-    val hoursInfo: List<HourlyInfo>,
+    val dateTime: String,
     val regionCode: String,
-    val nextPageToken: String?,
-    val code: String,
-    val displayName: String,
-    val aqi: Int,
-    val aqiDisplay: String,
-    val color: Color,
-    val category: String,
-    val dominantPollutant: String
+    val indexes: List<AirQualityIndex>,
+    val pollutants: List<Pollutant>,
+    val healthRecommendations: HealthRecommendations
+)
+
+
+//This is a data class for history. Should be moved elsewhere
+data class HistoryLookupRequest(
+    val pageSize: Int,
+    val pageToken: String,
+    val location: LatLng,
+    val extraComputations: List<ExtraComputation>,
+    val uaqiColorPalette: ColorPalette,
+    val customLocalAqis: List<CustomLocalAqi>,
+    val timeRange: TimeRange,
+    val universalAqi: Boolean,
+    val languageCode: String
+)
+
+data class HistoryLookupResponse(
+    val hoursInfo: List<HourInfo>,
+    val regionCode: String,
+    val nextPageToken: String
+)
+
+data class TimeRange(
+    val dateTime: String,
+    val hours: Int,
+    val period: Interval
+)
+
+data class Interval(
+    val startTime: String,
+    val endTime: String
 )
