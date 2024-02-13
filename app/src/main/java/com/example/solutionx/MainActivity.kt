3efc6e39eb1c62c3ad8b6fc812a9b8c941b66987
hamzariffic <hamza.mattea@gmail.com>
2024-02-13 19:13:18 +0300

    I've cleaned up the code in the main screen

--- app/src/main/java/com/example/solutionx/MainActivity.kt
+++ app/src/main/java/com/example/solutionx/MainActivity.kt
@@ -6,14 +6,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScopeInstance.weight
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Card
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.FloatingActionButton
@@ -24,18 +20,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.solutionx.UI.AirQualityScreen
-import com.example.solutionx.ViewModel.AirQualityViewModel
 import com.example.solutionx.ui.theme.SolutionXTheme
 
 class MainActivity : ComponentActivity() {
@@ -43,6 +34,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SolutionXTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = colorScheme.background
+                ) {
+                    Home(navController = rememberNavController())
+                }
                 val navController = rememberNavController()
                 MyApp(navController)
             }
@@ -52,47 +49,29 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp(navController: NavHostController) {
-    SolutionXTheme {
-        val viewModel: AirQualityViewModel = viewModel()    }
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = colorScheme.background
-        )
-        NavHost(navController = navController, startDestination = "greeting") {
-            composable("greeting") { Greeting(navController) }
+        NavHost(navController = navController, startDestination = "Home") {
+            composable("Home") { Home(navController) }
             composable("airQualityData") {
-//                AirQualityScreen(
-//                    viewModel = ViewModel,
-//                    latitude = 39.6682,
-//                    longitude = 4.04354
-//                )
         }
     }
 }
 
 
 @Composable
-fun Greeting(navController: NavController) {
-    listOf("Hamza", "Ali", "Ahmed")
+fun Home(navController: NavController) {
     val expanded = remember { mutableStateOf(false) }
-    val expanded2 = remember { mutableStateOf(false) }
-    val expanded3 = remember { mutableStateOf(false) }
-    val expanded4 = remember { mutableStateOf(false) }
 
-    Row( modifier = Modifier.weight(1f)) {
-//            Air Quality data
+    Row {
         OutlinedButton(onClick = {
             navController.navigate("AirQualityScreen")
         }) {
             Text(text = "Air\uD83D\uDCA8")
-
         }
         Spacer(modifier = Modifier.weight(0.3f))
 
-//      Solar intensity data
-        FilledTonalButton(onClick = { expanded2.value = !expanded2.value }) {
+        FilledTonalButton(onClick = { expanded.value = !expanded.value }) {
             Text(
-                text = if (expanded2.value) {
+                text = if (expanded.value) {
                     "Solar\uD83C\uDF1E"
                 } else {
                     "Solar intensity data"
@@ -100,26 +79,19 @@ fun Greeting(navController: NavController) {
         }
         Spacer(modifier = Modifier.weight(0.3f))
 
-//       Ocean data
         FloatingActionButton(onClick = {
-            expanded3.value = !expanded3.value
+            expanded.value = !expanded.value
         }) {
-            Text(text = if (expanded3.value) "Ocean" else "Ocean\uD83C\uDF0A")
+            Text(text = if (expanded.value) "Ocean" else "Ocean\uD83C\uDF0A")
         }
         Spacer(modifier = Modifier.weight(0.3f))
 
-        FilledTonalButton(onClick = {expanded4.value = !expanded4.value}) {
-            Text(text = if(expanded4.value) "AI" else "\uD83E\uDD16")
+        FilledTonalButton(onClick = { /*TODO*/ }) {
+            Text(text = "SDG")
         }
-        Spacer(modifier = Modifier.weight(1f))
-
-
-//            FilledIconButton(onClick = { /*TODO*/ }) {
-//                Text(text = "SDG")
-//            }
     }
+
     Column(modifier = Modifier.fillMaxWidth()) {
-//        This is the expandable button for the UI so far! It's kinda murky but I'm gonna clean it with time
         ElevatedButton(onClick = { expanded.value = !expanded.value }) {
             Text(
                 text = if (expanded.value) {
@@ -129,16 +101,13 @@ fun Greeting(navController: NavController) {
                 }
             )
         }
-
-//I've nested these buttons to where I need to add some onClick logic to perform specific actions!
     }
 }
 
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun HomePreview() {
     SolutionXTheme {
-        Greeting(navController = rememberNavController())
+        Home(navController = rememberNavController())
     }
 }
