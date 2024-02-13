e7959c454c11a97d5ace869358c0345421c3b1cf
hamzariffic <hamza.mattea@gmail.com>
2024-02-13 19:16:27 +0300

    The view model is responsible for handling the UI data so that it persitsts even during a configuration change

--- app/src/main/java/com/example/solutionx/ViewModel/ViewModel.kt
+++ app/src/main/java/com/example/solutionx/ViewModel/ViewModel.kt
@@ -5,24 +5,31 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.solutionx.APIService.AirQualityClient
 import com.example.solutionx.APIService.AirQualityResponse
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 
 // This view model fetches the air quality data and exposes it to the UI
 class AirQualityViewModel : ViewModel() {
-    private val airQualityClient = AirQualityClient.airQualityApiService
 
+    private val airQualityClient = AirQualityClient.airQualityApiService
     private val _airQualityResponse = mutableStateOf<AirQualityResponse?>(null)
     val airQualityResponse = _airQualityResponse
 
     fun fetchAirQuality(latitude: Double, longitude: Double) {
         viewModelScope.launch {
             try {
-                _airQualityResponse.value = airQualityClient.getCurrentConditions(latitude, longitude)
+                _airQualityResponse.value =
+                    airQualityClient.getCurrentConditions(latitude, longitude)
             } catch (e: Exception) {
                 // Handle error
                 println("Could not get data: ${e.message}")
             }
         }
+        // This method is called when the app is recreated after a configuration change
+        fun onCleared() {
+            super.onCleared()
+            // Cancel any running coroutines
+            viewModelScope.cancel()
+        }
     }
 }
-
