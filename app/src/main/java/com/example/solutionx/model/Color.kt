c9c586b76b7b0c921d28a24b518743d0b3730dcd
hamzariffic <hamza.mattea@gmail.com>
2024-02-17 06:16:08 +0300

    parsed the array of AQI colors in hex

--- app/src/main/java/com/example/solutionx/model/Color.kt
+++ app/src/main/java/com/example/solutionx/model/Color.kt
@@ -4,5 +4,7 @@ data class Color(
     val red: Int,
     val green: Int,
     val blue: Int,
-    val alpha: Int?
+    val alpha: Int?,
+
+  val colorList: List<String> = listOf("#000000", "#009966","#ffcc00", "#ff9900", "#cc0000", "#660066", "#9933cc")
 )
