1600a7ee2560589b338407eda5b7ff77f82f4319
hamzariffic <hamza.mattea@gmail.com>
2024-02-17 03:31:53 +0300

    data class for the air quality requests

--- app/src/main/java/com/example/solutionx/model/airQualityResponse.kt
+++ app/src/main/java/com/example/solutionx/model/airQualityResponse.kt
@@ -1,4 +1,12 @@
 package com.example.solutionx.model
 
-class airQualityResponse {
-}
+import com.google.type.LatLng
+
+data class AirQualityRequest(
+    val location: LatLng,
+    val extraComputations: List<ExtraComputation>,
+    val uaqiColorPalette: ColorPalette,
+    val customLocalAqis: List<CustomLocalAqi>,
+    val universalAqi: Boolean,
+    val languageCode: String
+)
