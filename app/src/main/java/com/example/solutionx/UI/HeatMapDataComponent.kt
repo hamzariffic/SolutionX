b7ce0fafede22496f644f15487f3661b6098cbfd
hamzariffic <hamza.mattea@gmail.com>
2024-02-17 03:55:29 +0300

    heat maps for solar tracking. Still under development

--- app/src/main/java/com/example/solutionx/UI/HeatMapDataComponent.kt
+++ app/src/main/java/com/example/solutionx/UI/HeatMapDataComponent.kt
@@ -1,4 +1,10 @@
 package com.example.solutionx.UI
 
-class HeatMapDataComponent {
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun <heatMapResponse> HeatmapDataComponent(heatmapResponse: heatMapResponse) {
+    Text(text = heatmapResponse.toString())
 }
