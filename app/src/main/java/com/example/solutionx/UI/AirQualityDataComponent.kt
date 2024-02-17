01273495a2973af55d0b98e05a322449c2d9f66c
hamzariffic <hamza.mattea@gmail.com>
2024-02-17 03:57:34 +0300

    Air quality data component from the response

--- app/src/main/java/com/example/solutionx/UI/AirQualityDataComponent.kt
+++ app/src/main/java/com/example/solutionx/UI/AirQualityDataComponent.kt
@@ -1,4 +1,12 @@
 package com.example.solutionx.UI
 
-class AirQualityDataComponent {
+import androidx.compose.material3.Card
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.example.solutionx.APIService.HourlyInfo
+
+@Composable
+fun AirQualityDataComponent(airQualityResponse: airQualityResponse) {
+    Text(text = airQualityResponse.toString())
 }
