b90e5f9622156e45b947c1fcc63cc8be364404ad
hamzariffic <hamza.mattea@gmail.com>
2024-02-17 03:41:09 +0300

    the enum class for extra computation of the identified objects

--- app/src/main/java/com/example/solutionx/model/ExtraComputation.kt
+++ app/src/main/java/com/example/solutionx/model/ExtraComputation.kt
@@ -1,4 +1,9 @@
 package com.example.solutionx.model
 
-class ExtraComputation {
+enum class ExtraComputation {
+    HEALTH_RECOMMENDATIONS,
+    DOMINANT_POLLUTANT_CONCENTRATION,
+    POLLUTANT_CONCENTRATION,
+    LOCAL_AQI,
+    POLLUTANT_ADDITIONAL_INFO
 }
