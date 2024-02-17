ab88d3c4291839ec86867694d12d30b8335963ac
hamzariffic <hamza.mattea@gmail.com>
2024-02-17 03:51:02 +0300

    the function that runs the app's composables

--- app/src/main/java/com/example/solutionx/UI/MyApp.kt
+++ app/src/main/java/com/example/solutionx/UI/MyApp.kt
@@ -1,4 +1,16 @@
 package com.example.solutionx.UI
 
-class MyApp {
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+@Composable
+fun MyApp(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "Home") {
+        composable("Home") { Home(navController) }
+        composable("airQualityData") {
+            // Your airQualityData composable content goes here
+        }
+    }
 }
