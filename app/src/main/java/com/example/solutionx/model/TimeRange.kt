5d40900cbd5fcb715b9c1437cc7248f0650829b8
hamzariffic <hamza.mattea@gmail.com>
2024-02-17 06:14:25 +0300

    TIme range

--- app/src/main/java/com/example/solutionx/model/TimeRange.kt
+++ app/src/main/java/com/example/solutionx/model/TimeRange.kt
@@ -1,10 +1,12 @@
 package com.example.solutionx.model
 
-import com.example.solutionx.APIService.Interval
-
 
 data class TimeRange(
     val dateTime: String,
     val hours: Int,
     val period: Interval
+)
+data class Interval(
+    val startTime: String,
+    val endTime: String
 )
