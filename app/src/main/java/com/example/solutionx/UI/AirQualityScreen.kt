fcbe78faa60dd87b003a5fd4ee96bde2381afd97
hamzariffic <hamza.mattea@gmail.com>
2024-02-17 03:56:32 +0300

    User will be navigated here onClick to view the air quality data

--- app/src/main/java/com/example/solutionx/UI/AirQualityScreen.kt
+++ app/src/main/java/com/example/solutionx/UI/AirQualityScreen.kt
@@ -1,22 +1,17 @@
-package com.example.solutionx.UI
-
+package com.example.solutionx.ui
 
 import androidx.compose.material3.Card
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
-import com.example.solutionx.APIService.HourlyInfo
+import com.example.solutionx.UI.AirQualityDataComponent
+import com.example.solutionx.UI.HeatmapDataComponent
+import com.example.solutionx.UI.HourlyHistoryDataComponent
 import com.example.solutionx.ViewModel.AirQualityViewModel
 
-
-//Fix this bug "    val airQualityResponse by viewModel.airQualityResponse.collectAsState()"
-//& also the bug when checking if data is available
 @Composable
 fun AirQualityScreen(navController: NavHostController, latitude: Double, longitude: Double) {
     val viewModel: AirQualityViewModel = viewModel()
@@ -31,13 +26,13 @@ fun AirQualityScreen(navController: NavHostController, latitude: Double, longitu
         println("Nothing here!")
     }
     Card() {
-//        Current conditions
+        // Current conditions
         ElevatedButton(onClick = { navController.navigate("home") }) {
             Text(text = "Back")
         }
     }
     Card() {
-//        Hourly history
+        // Hourly history
         ElevatedButton(onClick = {
             // Get the hourly history data from the API
             val hourlyHistoryResponse = viewModel.getHourlyHistory(latitude, longitude)
@@ -49,43 +44,15 @@ fun AirQualityScreen(navController: NavHostController, latitude: Double, longitu
         }
     }
     Card(){
-//        Heatmaps
+        // Heatmaps
         ElevatedButton(onClick = {
             // Get the heatmap data from the API
             val heatmapResponse = viewModel.getHeatmap(latitude, longitude)
 
             // Display the heatmap data using the provided UI components
-            HeatmapDataComponent(heatmapResponse!!)
+            HeatmapDataComponent<Any>(heatmapResponse!!)
         }) {
             Text(text = "Heatmaps")
         }
     }
-}
-
-@Composable
-fun HourlyHistoryDataComponent(hourlyHistoryResponse: HourlyInfo) {
-
-}
-
-
-@Composable
-fun HeatmapDataComponent(heatmapResponse: heatMapResponse) {
-    Text(text = heatmapResponse.toString())
-}
-
-@Composable
-fun AirQualityDataComponent(airQualityResponse: airQualityResponse) {
-    Text(text = airQualityResponse.toString())
-}
-
-//Current Conditions: Real-time hourly air quality information.
-//Hourly History: Air quality history for a specific location, for a given time range, up to a maximum of 30 days.
-//Heatmaps: Color coded tiles of various indexes and pollutants.
-
-
-@Preview(showBackground = true)
-@Composable
-fun AirQualityScreenPreview() {
-    val navController = rememberNavController()
-    AirQualityScreen(navController, 39.6682, 4.04354)
 }
