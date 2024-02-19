f83b648958a89443bc8953ceb6750f9003600819
hamzariffic <hamza.mattea@gmail.com>
2024-02-20 01:39:48 +0300

    I want to separate business logic, UI, and data using the VM

--- app/src/main/java/com/example/solutionx/model/AirQualityViewModel.kt
+++ app/src/main/java/com/example/solutionx/model/AirQualityViewModel.kt
@@ -1,4 +1,29 @@
 package com.example.solutionx.model
 
-class AirQualityViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.solutionx.APIService.AirQualityApiService
+import kotlinx.coroutines.launch
+
+class AirQualityViewModel(private val apiService: AirQualityApiService) : ViewModel() {
+    private val _airQualityData = MutableLiveData<AirQualityResponse>()
+    val airQualityData: LiveData<AirQualityResponse> get() = _airQualityData
+
+    fun fetchAirQualityData(airQualityRequest: AirQualityRequest) {
+        viewModelScope.launch {
+            try {
+                val call = apiService.getCurrentConditions(airQualityRequest)
+                val response = call.awaitResponse()
+
+                if (response.isSuccessful) {
+                    _airQualityData.value = response.body()
+                } else {
+                    println("Could not get Air Quality Conditions")
+                }
+            } catch (e: Exception) {
+                println("Could not get Air Quality Conditions")}
+        }
+    }
 }
