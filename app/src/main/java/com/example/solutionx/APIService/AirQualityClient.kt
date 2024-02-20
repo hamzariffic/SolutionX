57bda91e98113032525f5b898d784ae13f265ba5
hamzariffic <hamza.mattea@gmail.com>
2024-02-20 03:31:04 +0300

    All the endpoints call from this client

--- app/src/main/java/com/example/solutionx/APIService/AirQualityClient.kt
+++ app/src/main/java/com/example/solutionx/APIService/AirQualityClient.kt
@@ -3,17 +3,20 @@ package com.example.solutionx.APIService
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-//This is the API's air quality client. It takes in the initial part of the API's URL
-
 object AirQualityClient {
     private const val BASE_URL = "https://airquality.googleapis.com/v1/currentConditions:lookup?key=AIzaSyAiDHeFyGtYPO8Rz5ZvR__fp1TEfAQRKck"
+    private const val BASE_URL_History = "https://airquality.googleapis.com/v1/history:lookup?key=AIzaSyAiDHeFyGtYPO8Rz5ZvR__fp1TEfAQRKck"
 
-//    currentConditions:lookup?
     private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
-        .addCallAdapterFactory(CoroutineCallAdapterFactory())
+        .build()
+
+    private val retrofitHistory = Retrofit.Builder()
+        .baseUrl(BASE_URL_History)
+        .addConverterFactory(GsonConverterFactory.create())
         .build()
 
     val airQualityApiService: AirQualityApiService = retrofit.create(AirQualityApiService::class.java)
+    val airQualityHistoryApiService: AirQualityApiService = retrofitHistory.create(AirQualityApiService::class.java)
 }
