d25617f94e26e31786fb00ceeddf89581c337ec6
hamzariffic <hamza.mattea@gmail.com>
2024-02-20 23:19:57 +0300

    look up parameters

--- app/src/main/java/com/example/solutionx/model/HistoryLookupRequest.kt
+++ app/src/main/java/com/example/solutionx/model/HistoryLookupRequest.kt
@@ -1,12 +1,11 @@
 package com.example.solutionx.model
 
 import com.example.solutionx.APIService.ColorPalette
-import com.google.type.LatLng
 
 data class HistoryLookupRequest(
     val pageSize: Int,
     val pageToken: String,
-    val location: LatLng,
+    val location: com.google.android.gms.maps.model.LatLng,
     val extraComputations: List<ExtraComputation>,
     val uaqiColorPalette: ColorPalette,
     val customLocalAqis: List<CustomLocalAqi>,
