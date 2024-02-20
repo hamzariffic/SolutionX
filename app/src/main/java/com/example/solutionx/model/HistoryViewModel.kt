baceb516cbc2a9ec63324492578fc1948d047b1b
hamzariffic <hamza.mattea@gmail.com>
2024-02-20 03:28:17 +0300

    view model handling logic for history enpoint

--- app/src/main/java/com/example/solutionx/model/HistoryViewModel.kt
+++ app/src/main/java/com/example/solutionx/model/HistoryViewModel.kt
@@ -1,4 +1,13 @@
 package com.example.solutionx.model
 
-class HistoryViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.solutionx.APIService.AirQualityApiService
+import com.example.solutionx.APIService.HistoryRequest
+
+class HistoryViewModel<HistoryResponse> : ViewModel() {
+    private val airQualityApiService = AirQualityApiService.create()
+
+    suspend fun fetchAirQualityHistory(request: HistoryRequest): HistoryResponse {
+        return airQualityApiService.getAirQualityHistory(request)
+    }
 }
