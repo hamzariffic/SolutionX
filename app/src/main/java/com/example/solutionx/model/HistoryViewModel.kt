d2536c3a3ce83536e06be319627507faa2c31fa4
hamzariffic <hamza.mattea@gmail.com>
2024-02-20 08:51:44 +0300

    Fixed the .create() function by extending the API service with a companion object

--- app/src/main/java/com/example/solutionx/model/HistoryViewModel.kt
+++ app/src/main/java/com/example/solutionx/model/HistoryViewModel.kt
@@ -2,12 +2,13 @@ package com.example.solutionx.model
 
 import androidx.lifecycle.ViewModel
 import com.example.solutionx.APIService.AirQualityApiService
+import com.example.solutionx.APIService.HistoryLookupResponse
 import com.example.solutionx.APIService.HistoryRequest
 
 class HistoryViewModel<HistoryResponse> : ViewModel() {
     private val airQualityApiService = AirQualityApiService.create()
 
-    suspend fun fetchAirQualityHistory(request: HistoryRequest): HistoryResponse {
+    suspend fun fetchAirQualityHistory(request: HistoryRequest): HistoryLookupResponse {
         return airQualityApiService.getAirQualityHistory(request)
     }
 }
