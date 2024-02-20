669e289406707bfb9d60df7829ba2a4f32fa37cd
hamzariffic <hamza.mattea@gmail.com>
2024-02-20 08:07:53 +0300

    removed a redundant interface for the airQuality client that existed

--- app/src/main/java/com/example/solutionx/APIService/AirQualityApiService.kt
+++ app/src/main/java/com/example/solutionx/APIService/AirQualityApiService.kt
@@ -1,6 +1,5 @@
 package com.example.solutionx.APIService
 
-import com.example.solutionx.model.AirQualityRequest
 import com.example.solutionx.model.AirQualityResponse
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -10,7 +9,7 @@ import retrofit2.http.POST
 interface AirQualityApiService {
     @POST("currentConditions:lookup")
     suspend fun currentConditions(
-        @Body airQualityRequest: AirQualityRequest
+        @Body airQualityRequest: String
     ): AirQualityResponse
 
     @POST("history:lookup")
@@ -21,14 +20,14 @@ interface AirQualityApiService {
     companion object
 }
 
-object AirQualityClient {
-    private const val BASE_URL = "https://airquality.googleapis.com/v1/"
-
-    private val retrofit: Retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-
-    val airQualityApiService: AirQualityApiService = retrofit.create(AirQualityApiService::class.java)
-}
+//object AirQualityClient {
+//    private const val BASE_URL = "https://airquality.googleapis.com/v1/"
+//
+//    private val retrofit: Retrofit = Retrofit.Builder()
+//        .baseUrl(BASE_URL)
+//        .addConverterFactory(GsonConverterFactory.create())
+//        .build()
+//
+//    val airQualityApiService: AirQualityApiService = retrofit.create(AirQualityApiService::class.java)
+//}
 
