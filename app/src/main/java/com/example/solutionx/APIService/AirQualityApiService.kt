675fd83d6df81eb14e1dccc16820d95b15ae7c4d
hamzariffic <hamza.mattea@gmail.com>
2024-02-20 03:25:29 +0300

    Updated the POST request for the history endpoint

--- app/src/main/java/com/example/solutionx/APIService/AirQualityApiService.kt
+++ app/src/main/java/com/example/solutionx/APIService/AirQualityApiService.kt
@@ -2,13 +2,18 @@ package com.example.solutionx.APIService
 
 import com.example.solutionx.model.AirQualityRequest
 import com.example.solutionx.model.AirQualityResponse
+import com.example.solutionx.model.HistoryResponse
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AirQualityApiService {
 //    The current endpoint defines the location of the individual to provide the air quality data
     @POST("currentConditions:lookup?")
-    suspend fun getCurrentConditions(
+    suspend fun CurrentConditions(
         @Body airQualityRequest: AirQualityRequest
         ): AirQualityResponse
+    @POST("history:lookup")
+    suspend fun getAirQualityHistory(
+        @Body request: HistoryRequest
+    ): HistoryResponse
 }
