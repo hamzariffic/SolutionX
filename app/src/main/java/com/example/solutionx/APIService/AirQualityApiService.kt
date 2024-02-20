e3bfae9c36971022a51a7ddeb894ab15d0217c96
hamzariffic <hamza.mattea@gmail.com>
2024-02-20 05:50:57 +0300

    code optimized a little bit

--- app/src/main/java/com/example/solutionx/APIService/AirQualityApiService.kt
+++ app/src/main/java/com/example/solutionx/APIService/AirQualityApiService.kt
@@ -2,18 +2,33 @@ package com.example.solutionx.APIService
 
 import com.example.solutionx.model.AirQualityRequest
 import com.example.solutionx.model.AirQualityResponse
-import com.example.solutionx.model.HistoryResponse
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AirQualityApiService {
-//    The current endpoint defines the location of the individual to provide the air quality data
-    @POST("currentConditions:lookup?")
-    suspend fun CurrentConditions(
+    @POST("currentConditions:lookup")
+    suspend fun currentConditions(
         @Body airQualityRequest: AirQualityRequest
-        ): AirQualityResponse
+    ): AirQualityResponse
+
     @POST("history:lookup")
     suspend fun getAirQualityHistory(
-        @Body request: HistoryRequest
-    ): HistoryResponse
+        @Body historyLookupRequest: HistoryLookupRequest
+    ): HistoryLookupResponse
+
+    companion object
 }
+
+object AirQualityClient {
+    private const val BASE_URL = "https://airquality.googleapis.com/v1/"
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val airQualityApiService: AirQualityApiService = retrofit.create(AirQualityApiService::class.java)
+}
+
