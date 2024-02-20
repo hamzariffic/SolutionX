6cb74608e9b045e2be4d2ce43ef5bcd6500fe6ed
hamzariffic <hamza.mattea@gmail.com>
2024-02-20 23:21:29 +0300

    History data components slotted hourly

--- app/src/main/java/com/example/solutionx/UI/HourlyHistoryDataComponent.kt
+++ app/src/main/java/com/example/solutionx/UI/HourlyHistoryDataComponent.kt
@@ -1,9 +1,36 @@
 package com.example.solutionx.UI
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import com.example.solutionx.APIService.HourlyInfo
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.solutionx.model.Color
 
 @Composable
-fun HourlyHistoryDataComponent(hourlyHistoryResponse: HourlyInfo) {
+fun HourlyHistoryDataComponent(hourlyHistoryResponse: List<com.example.solutionx.model.HourlyInfo>) {
+    // Display the list of HourlyInfo data in an area
+    for (hourlyInfo in hourlyHistoryResponse) {
+        Text(text = hourlyInfo.toString())
+    }
+}
 
+@Preview
+@Composable
+fun PreviewHourlyHistoryDataComponent() {
+    // Passing a sample HourlyInfo data for preview
+    val sampleHourlyInfo = listOf(
+        com.example.solutionx.model.HourlyInfo(
+            "2024-02-20T15:00:00Z", listOf(
+                com.example.solutionx.model.Index(
+                    "uaqi",
+                    "Universal AQI",
+                    73,
+                    "73",
+                    Color(118, 202, 51, 255),
+                    "Good air quality",
+                    "pm10"
+                )
+            )
+        )
+    )
+    HourlyHistoryDataComponent(sampleHourlyInfo)
 }
