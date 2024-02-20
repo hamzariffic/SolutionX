569c6ee6b4f0299a4ed349acc88fb144cdf14ccd
hamzariffic <hamza.mattea@gmail.com>
2024-02-20 03:28:37 +0300

    View model for air quality

--- app/src/main/java/com/example/solutionx/model/AirQualityViewModel.kt
+++ app/src/main/java/com/example/solutionx/model/AirQualityViewModel.kt
@@ -1,11 +1,14 @@
 package com.example.solutionx.model
 
+import android.annotation.SuppressLint
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.solutionx.APIService.AirQualityApiService
+import com.google.android.gms.location.LocationServices
 import kotlinx.coroutines.launch
+import retrofit2.awaitResponse
 
 class AirQualityViewModel(private val apiService: AirQualityApiService) : ViewModel() {
     private val _airQualityData = MutableLiveData<AirQualityResponse>()
@@ -14,7 +17,7 @@ class AirQualityViewModel(private val apiService: AirQualityApiService) : ViewMo
     fun fetchAirQualityData(airQualityRequest: AirQualityRequest) {
         viewModelScope.launch {
             try {
-                val call = apiService.getCurrentConditions(airQualityRequest)
+                val call = apiService.CurrentConditions(airQualityRequest)
                 val response = call.awaitResponse()
 
                 if (response.isSuccessful) {
@@ -24,6 +27,21 @@ class AirQualityViewModel(private val apiService: AirQualityApiService) : ViewMo
                 }
             } catch (e: Exception) {
                 println("Could not get Air Quality Conditions")}
+        }
+    }
+}
+
+class LocationViewModel : ViewModel() {
+
+    private val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
+
+    private val _userLocation = MutableLiveData<Location>()
+    val userLocation: LiveData<Location> = _userLocation
+
+    @SuppressLint("MissingPermission")
+    fun fetchUserLocation() {
+        fusedLocationClient.lastLocation.addOnSuccessListener {
+            _userLocation.value = Location(it.latitude, it.longitude)
         }
     }
 }
