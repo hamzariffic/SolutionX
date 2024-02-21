480b2cce18e9291076197fb3864f4659e2b89c16
hamzariffic <hamza.mattea@gmail.com>
2024-02-22 01:12:38 +0300

    added HeatMaps object and airQualityHeatMapApiService

--- app/src/main/java/com/example/solutionx/APIService/AirQualityClient.kt
+++ app/src/main/java/com/example/solutionx/APIService/AirQualityClient.kt
@@ -6,18 +6,29 @@ import retrofit2.converter.gson.GsonConverterFactory
 object AirQualityClient {
     private const val BASE_URL = "https://airquality.googleapis.com/v1/currentConditions:lookup?key=YOUR_API_KEY"
     private const val BASE_URL_History = "https://airquality.googleapis.com/v1/history:lookup?key=YOUR_API_KEY"
+    private const val BASE_URL_HeatMaps = "https://airquality.googleapis.com/v1/"
 
+
+//  AirQuality Client
     private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
+//
     private val retrofitHistory = Retrofit.Builder()
         .baseUrl(BASE_URL_History)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
+//  HeatMap Client
+    private val retrofitHeatMaps = Retrofit.Builder()
+        .baseUrl(BASE_URL_HeatMaps)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
     val airQualityApiService: AirQualityApiService = retrofit.create(AirQualityApiService::class.java)
     val airQualityHistoryApiService: AirQualityApiService = retrofitHistory.create(AirQualityApiService::class.java)
+    val airQualityHeatMapApiService: AirQualityApiService = retrofitHeatMaps.create(AirQualityApiService::class.java)
 }
 
