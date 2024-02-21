3c935339c34160768bd83a39307ef66c60515bcf
hamzariffic <hamza.mattea@gmail.com>
2024-02-21 07:31:05 +0300

    Removed Call interface when making the AirQualityResponse data class for API response model

--- app/src/main/java/com/example/solutionx/model/AirQualityResponse.kt
+++ app/src/main/java/com/example/solutionx/model/AirQualityResponse.kt
@@ -1,47 +1,9 @@
 package com.example.solutionx.model
 
-import okhttp3.Request
-import okio.Timeout
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-
 data class AirQualityResponse(
     val dateTime: String,
     val regionCode: String,
     val indexes: List<Index>,
     val pollutants: List<Pollutant<Any?>>,
     val healthRecommendations: HealthRecommendations
-) : Call<AirQualityResponse> {
-    override fun clone(): Call<AirQualityResponse> {
-        TODO("Not yet implemented")
-    }
-
-    override fun execute(): Response<AirQualityResponse> {
-        TODO("Not yet implemented")
-    }
-
-    override fun enqueue(callback: Callback<AirQualityResponse>) {
-        TODO("Not yet implemented")
-    }
-
-    override fun isExecuted(): Boolean {
-        TODO("Not yet implemented")
-    }
-
-    override fun cancel() {
-        TODO("Not yet implemented")
-    }
-
-    override fun isCanceled(): Boolean {
-        TODO("Not yet implemented")
-    }
-
-    override fun request(): Request {
-        TODO("Not yet implemented")
-    }
-
-    override fun timeout(): Timeout {
-        TODO("Not yet implemented")
-    }
-}
+)
