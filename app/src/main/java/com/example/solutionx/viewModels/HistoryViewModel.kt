144c4e86379f724625e72a321a9bb478c95db7ac
hamzariffic <hamza.mattea@gmail.com>
2024-02-22 02:16:52 +0300

    History updates oncoming

--- app/src/main/java/com/example/solutionx/viewModels/HistoryViewModel.kt
+++ app/src/main/java/com/example/solutionx/viewModels/HistoryViewModel.kt
@@ -1,9 +1,10 @@
-package com.example.solutionx.model
+package com.example.solutionx.viewModels
 
 import androidx.lifecycle.ViewModel
 import com.example.solutionx.APIService.AirQualityApiService
 import com.example.solutionx.APIService.AirQualityClient.airQualityHistoryApiService
 import com.example.solutionx.APIService.HourlyInfo
+import com.example.solutionx.model.HistoryLookupRequest
 
 class HistoryViewModel : ViewModel() {
     private val airQualityApiService = AirQualityApiService.create()
