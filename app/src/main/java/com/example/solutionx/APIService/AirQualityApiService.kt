c81c72e0a746adbc2e88f47c7a5a72d6aabe0321
hamzariffic <hamza.mattea@gmail.com>
2024-02-22 01:10:53 +0300

    Added the HeatMap endpoint

--- app/src/main/java/com/example/solutionx/APIService/AirQualityApiService.kt
+++ app/src/main/java/com/example/solutionx/APIService/AirQualityApiService.kt
@@ -1,26 +1,40 @@
 package com.example.solutionx.APIService
 
+import com.example.solutionx.model.AirQualityRequest
 import com.example.solutionx.model.AirQualityResponse
+import com.example.solutionx.model.HistoryLookupRequest
+import okhttp3.ResponseBody
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface AirQualityApiService {
     @POST("currentConditions:lookup")
     suspend fun currentConditions(
-        @Body airQualityRequest: String
+        @Body airQualityRequest: AirQualityRequest
     ): AirQualityResponse
 
     @POST("history:lookup")
     suspend fun getAirQualityHistory(
-        @Body historyLookupRequest: HistoryRequest
+        @Body historyLookupRequest: HistoryLookupRequest
     ): List<HourlyInfo>
 
-    suspend fun getHourlyAirQualityHistory(historyRequest: HistoryRequest): List<HourlyInfo>
+    @GET("heatmapTiles/{type}/{zoom}/{x}/{y}")
+    suspend fun getHeatmapTile(
+        @Path("type") type: String,
+        @Path("zoom") zoom: Int,
+        @Path("x") x: Int,
+        @Path("y") y: Int
+    ): ResponseBody
+
+
+//    suspend fun getHourlyAirQualityHistory(historyRequest: HistoryRequest): List<HourlyInfo>
 
     companion object {
-        private const val BASE_URL = "https://your_base_url_here/"
+        private const val BASE_URL = "https://airquality.googleapis.com/v1/currentConditions:lookup?key"
 
         fun create(): AirQualityApiService {
             val retrofit = Retrofit.Builder()
