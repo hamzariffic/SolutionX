bf63fc9e5850b470659039142fd72d7be5369b40
hamzariffic <hamza.mattea@gmail.com>
2024-02-21 14:15:54 +0300

    air quality response

--- app/src/main/java/com/example/solutionx/model/AirQualityResponse.kt
+++ app/src/main/java/com/example/solutionx/model/AirQualityResponse.kt
@@ -1,9 +1,15 @@
 package com.example.solutionx.model
 
+import retrofit2.Callback
+
 data class AirQualityResponse(
     val dateTime: String,
     val regionCode: String,
     val indexes: List<Index>,
     val pollutants: List<Pollutant<Any?>>,
     val healthRecommendations: HealthRecommendations
-)
+) {
+    fun enqueue(callback: Callback<AirQualityResponse>) {
+        TODO("Not yet implemented")
+    }
+}
