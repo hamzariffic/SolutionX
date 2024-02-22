5dc6826dd0b0d5f0650ae62b2cc8039f9015f2e9
hamzariffic <hamza.mattea@gmail.com>
2024-02-22 04:20:01 +0300

    HeatMapTiles

--- app/src/main/java/com/example/solutionx/model/HeatMapTiles.kt
+++ app/src/main/java/com/example/solutionx/model/HeatMapTiles.kt
@@ -1,4 +1,43 @@
 package com.example.solutionx.model
 
-class HeatMapTiles {
+data class HeatmapTileRequest(
+    val mapType: MapType,
+    val zoom: Int,
+    val x: Int,
+    val y: Int
+)
+
+enum class MapType {
+    UAQI_RED_GREEN,
+    UAQI_INDIGO_PERSIAN,
+    PM25_INDIGO_PERSIAN,
+    GBR_DEFRA,
+    DEU_UBA,
+    CAN_EC,
+    FRA_ATMO,
+    US_AQI
+}
+
+data class HeatmapTileResponse(
+    val contentType: String,
+    val data: ByteArray,
+    val extensions: List<Any>
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as HeatmapTileResponse
+
+        if (contentType != other.contentType) return false
+        if (!data.contentEquals(other.data)) return false
+        return extensions == other.extensions
+    }
+
+    override fun hashCode(): Int {
+        var result = contentType.hashCode()
+        result = 31 * result + data.contentHashCode()
+        result = 31 * result + extensions.hashCode()
+        return result
+    }
 }
