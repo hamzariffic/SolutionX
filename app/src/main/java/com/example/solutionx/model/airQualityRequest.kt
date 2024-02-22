af65d6e4ed5b6313a0bd1595f6d9e5fc9d256cd6
hamzariffic <hamza.mattea@gmail.com>
2024-02-22 04:23:32 +0300

    Revised the airQualityRequest codebase

--- app/src/main/java/com/example/solutionx/model/airQualityRequest.kt
+++ app/src/main/java/com/example/solutionx/model/airQualityRequest.kt
@@ -1,12 +1,10 @@
 package com.example.solutionx.model
 
-import com.example.solutionx.APIService.ColorPalette
 import com.google.type.LatLng
 
 data class AirQualityRequest(
     val location: LatLng,
     val extraComputations: List<ExtraComputation>,
-    val uaqiColorPalette: ColorPalette,
     val customLocalAqis: List<CustomLocalAqi>,
     val universalAqi: Boolean,
     val languageCode: String
