ee382356dffaaf8ec335b751c825a422977d0e41
hamzariffic <hamza.mattea@gmail.com>
2024-02-22 04:18:22 +0300

    LocationViewModel

--- app/src/main/java/com/example/solutionx/viewModels/LocationViewModel.kt
+++ app/src/main/java/com/example/solutionx/viewModels/LocationViewModel.kt
@@ -1,4 +1,24 @@
 package com.example.solutionx.viewModels
 
-class LocationViewModel {
+import android.annotation.SuppressLint
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.solutionx.model.Location
+import com.google.android.gms.location.LocationServices
+
+class LocationViewModel(context: Context) : ViewModel() {
+
+    private val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
+
+    private val _userLocation = MutableLiveData<Location>()
+    val userLocation: LiveData<Location> = _userLocation
+
+    @SuppressLint("MissingPermission")
+    fun fetchUserLocation() {
+        fusedLocationClient.lastLocation.addOnSuccessListener {
+            _userLocation.value = Location(it.latitude, it.longitude)
+        }
+    }
 }
