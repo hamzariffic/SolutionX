4c2dc36dcfb91bf92403d86d1699a01920f016ae
hamzariffic <hamza.mattea@gmail.com>
2024-02-22 04:20:51 +0300

    enum class was usable when I called ExtraComputation.enumerate

--- app/src/main/java/com/example/solutionx/model/ExtraComputation.kt
+++ app/src/main/java/com/example/solutionx/model/ExtraComputation.kt
@@ -1,5 +1,5 @@
 package com.example.solutionx.model
-public enum class ExtraComputation {
+enum class ExtraComputation {
     HEALTH_RECOMMENDATIONS,
     DOMINANT_POLLUTANT_CONCENTRATION,
     POLLUTANT_CONCENTRATION,
