920c5b3c4804447c5e1571c92e30e412177e41a1
hamzariffic <hamza.mattea@gmail.com>
2024-02-22 07:57:09 +0300

    modified extra computations

--- app/src/main/java/com/example/solutionx/model/ExtraComputation.kt
+++ app/src/main/java/com/example/solutionx/model/ExtraComputation.kt
@@ -1,8 +1,30 @@
 package com.example.solutionx.model
-enum class ExtraComputation {
-    HEALTH_RECOMMENDATIONS,
-    DOMINANT_POLLUTANT_CONCENTRATION,
-    POLLUTANT_CONCENTRATION,
-    LOCAL_AQI,
-    POLLUTANT_ADDITIONAL_INFO
+sealed class ExtraComputation {
+    data object HEALTH_RECOMMENDATIONS : ExtraComputation()
+    data object DOMINANT_POLLUTANT_CONCENTRATION : ExtraComputation()
+    data object POLLUTANT_CONCENTRATION : ExtraComputation()
+    data object LOCAL_AQI : ExtraComputation()
+    data object POLLUTANT_ADDITIONAL_INFO : ExtraComputation()
+    companion object {
+        fun values(): Array<ExtraComputation> {
+            return arrayOf(
+                HEALTH_RECOMMENDATIONS,
+                DOMINANT_POLLUTANT_CONCENTRATION,
+                POLLUTANT_CONCENTRATION,
+                LOCAL_AQI,
+                POLLUTANT_ADDITIONAL_INFO
+            )
+        }
+
+        fun valueOf(value: String): ExtraComputation {
+            return when (value) {
+                "HEALTH_RECOMMENDATIONS" -> HEALTH_RECOMMENDATIONS
+                "DOMINANT_POLLUTANT_CONCENTRATION" -> DOMINANT_POLLUTANT_CONCENTRATION
+                "POLLUTANT_CONCENTRATION" -> POLLUTANT_CONCENTRATION
+                "LOCAL_AQI" -> LOCAL_AQI
+                "POLLUTANT_ADDITIONAL_INFO" -> POLLUTANT_ADDITIONAL_INFO
+                else -> throw IllegalArgumentException("No object com.example.solutionx.model.ExtraComputation.$value")
+            }
+        }
+    }
 }
