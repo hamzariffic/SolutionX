ab6894e3e1af34d1c15906019a5f04deccc38f42
hamzariffic <hamza.mattea@gmail.com>
2024-02-22 04:24:20 +0300

    Passed more screens to the NavGraph

--- app/src/main/java/com/example/solutionx/UI/MyApp.kt
+++ app/src/main/java/com/example/solutionx/UI/MyApp.kt
@@ -4,22 +4,29 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-//import  com.example.solutionx.UI.HeatmapDataComponent
 
 @Composable
 fun MyApp(navController: NavHostController) {
     NavHost(navController = navController, startDestination = "Home") {
         composable("Home") { Home(navController) }
         composable("airQualityDataComponent") {
-            AirQualityDataComponent()        }
+            AirQualityDataComponent()
+            HourlyHistoryDataComponent(hourlyHistoryResponse = listOf())
+        }
         composable("HourlyHistoryDataComponent") {
             HourlyHistoryDataComponent(hourlyHistoryResponse = listOf())
         }
-        composable("HeatMapData") {
-//            HeatmapDataComponent()
+        composable("HeatMapData/{type}/{zoom}/{x}/{y}") { backStackEntry ->
+            val type = backStackEntry.arguments?.getString("type") ?: ""
+            val zoom = backStackEntry.arguments?.getInt("zoom") ?: 0
+            val x = backStackEntry.arguments?.getInt("x") ?: 0
+            val y = backStackEntry.arguments?.getInt("y") ?: 0
+            HeatmapComponent(type, zoom, x, y)
         }
         composable("AirQualityDataComponent") {
             AirQualityDataComponent()        }
-//        composable("")
+        composable("HistoryScreen"){
+            HistoryScreen()
+        }
     }
 }
