448b980128f7980d6f027b4e093de51fc84b8598
hamzariffic <hamza.mattea@gmail.com>
2024-02-24 02:36:59 +0300

    index of air quality

--- app/src/main/java/com/example/solutionx/model/index.kt
+++ app/src/main/java/com/example/solutionx/model/index.kt
@@ -1,14 +1,49 @@
 package com.example.solutionx.model
 
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.compose.ui.graphics.Color
 
-//Contains air quality indices
+// Contains air quality indices
 data class Index(
     val code: String?,
     val displayName: String?,
     val aqi: Int,
     val aqiDisplay: String?,
-    val color: Color,
-    val category: String,
-    val dominantPollutant: String
-)
+    val color: Color?,
+    val category: String?,
+    val dominantPollutant: String?
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readInt(),
+        parcel.readString(),
+        Color(0),
+        parcel.readString(),
+        parcel.readString()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(code)
+        parcel.writeString(displayName)
+        parcel.writeInt(aqi)
+        parcel.writeString(aqiDisplay)
+        parcel.writeString(category)
+        parcel.writeString(dominantPollutant)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Index> {
+        override fun createFromParcel(parcel: Parcel): Index {
+            return Index(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Index?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
