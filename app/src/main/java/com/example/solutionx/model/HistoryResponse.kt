fc4c4eb5741ff8980d2a4f7fd26337cfa7444f6c
hamzariffic <hamza.mattea@gmail.com>
2024-02-24 02:37:20 +0300

    history response

--- app/src/main/java/com/example/solutionx/model/HistoryResponse.kt
+++ app/src/main/java/com/example/solutionx/model/HistoryResponse.kt
@@ -1,4 +1,64 @@
 package com.example.solutionx.model
 
-class HistoryResponse {
+import android.os.Parcel
+import android.os.Parcelable
+
+data class HistoryResponse(
+    val hoursInfo: List<HourlyInfo>,
+    val regionCode: String,
+    val nextPageToken: String? = null
+)
+
+data class HourlyInfo(
+    val dateTime: String,
+    val indexes: List<Index>
+)
+
+data class Index(
+    val code: Parcel,
+    val displayName: String,
+    val aqi: Int,
+    val aqiDisplay: String,
+    val color: Color,
+    val category: String,
+    val dominantPollutant: String
+) {
+    constructor(code: Parcel) : this(
+    )
+}
+
+data class Color(
+    val red: Int,
+    val green: Int,
+    val blue: Int,
+    val alpha: Int
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readInt(),
+        parcel.readInt(),
+        parcel.readInt()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(red)
+        parcel.writeInt(green)
+        parcel.writeInt(blue)
+        parcel.writeInt(alpha)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Color> {
+        override fun createFromParcel(parcel: Parcel): Color {
+            return Color()
+        }
+
+        override fun newArray(size: Int): Array<Color?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
