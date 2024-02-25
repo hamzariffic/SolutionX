cf67d339a8d425f5fae1b5fdf2ae6c3692a9ebc0
hamzariffic <hamza.mattea@gmail.com>
2024-02-25 17:06:10 +0300

    added the hourlyInfo model

--- app/src/main/java/com/example/solutionx/model/hourlyInfo.kt
+++ app/src/main/java/com/example/solutionx/model/hourlyInfo.kt
@@ -1,5 +1,35 @@
 package com.example.solutionx.model
+
+import android.os.Parcel
+import android.os.Parcelable
+import java.util.ArrayList
+
 data class HourlyInfo(
-    val dateTime: String,
-    val indexes: List<Index>
-)
+    val dateTime: String?,
+    val indexes: ArrayList<Index>?
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.createTypedArrayList(Index)
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(dateTime)
+        parcel.writeTypedList(indexes)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<HourlyInfo> {
+        override fun createFromParcel(parcel: Parcel): HourlyInfo {
+            return HourlyInfo(parcel)
+        }
+
+        override fun newArray(size: Int): Array<HourlyInfo?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
