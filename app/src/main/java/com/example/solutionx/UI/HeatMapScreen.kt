9d71cd5a366bf98b3f5633ad9af478ab72e3e4e0
hamzariffic <hamza.mattea@gmail.com>
2024-02-25 17:07:26 +0300

    fixed all bugs

--- app/src/main/java/com/example/solutionx/UI/HeatMapScreen.kt
+++ app/src/main/java/com/example/solutionx/UI/HeatMapScreen.kt
@@ -1,4 +1,28 @@
 package com.example.solutionx.UI
 
-class HeatMapScreen {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import com.example.solutionx.model.MapType
+
+@Composable
+fun HeatmapScreen() {
+    var showHeatmap by remember { mutableStateOf(false) }
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        Button(onClick = { showHeatmap = !showHeatmap }) {
+            Text(if (showHeatmap) "Hide Heatmap" else "Show Heatmap")
+        }
+
+        if (showHeatmap) {
+            HeatmapComponent(type = MapType.US_AQI, zoom = 2, x = 0, y = 1)
+        }
+    }
 }
