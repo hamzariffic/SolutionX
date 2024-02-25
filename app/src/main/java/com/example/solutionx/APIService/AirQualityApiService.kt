cb2aebbac9399445209d5c827de84f0af5c71cef
hamzariffic <hamza.mattea@gmail.com>
2024-02-26 00:20:36 +0300

    included companion objects for each interface

--- app/src/main/java/com/example/solutionx/APIService/AirQualityApiService.kt
+++ app/src/main/java/com/example/solutionx/APIService/AirQualityApiService.kt
@@ -2,7 +2,6 @@
 
 package com.example.solutionx.APIService
 
-import com.example.solutionx.model.AirQualityRequest
 import com.example.solutionx.model.AirQualityResponse
 import com.example.solutionx.model.HeatmapTileResponse
 import retrofit2.Retrofit
@@ -15,14 +14,42 @@ import retrofit2.http.Path
 interface AirQualityApiService {
     @POST("currentConditions:lookup=API_KEY")
     suspend fun currentConditions(
-        @Body airQualityRequest: AirQualityRequest
+        @Body airQualityRequest: String
     ): AirQualityResponse
 
+    fun getAirQualityHistory(historyRequest: HistoryLookupRequest): List<HistoryLookupRequest>
+
+    companion object {
+        private const val BASE_URL_History = "https://airquality.googleapis.com/v1/"
+
+        fun create(): AirQualityApiService {
+            val retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL_History)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            return retrofit.create(AirQualityApiService::class.java)
+        }
+    }}
+
+interface History{
     @POST("history:lookup=API_KEY")
     suspend fun getAirQualityHistory(
         @Body historyLookupRequest: Unit
     ): List<HistoryLookupRequest>
 
+    companion object {
+        private const val BASE_URL = "https://airquality.googleapis.com/v1/"
+
+        fun create(): AirQualityApiService {
+            val retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            return retrofit.create(AirQualityApiService::class.java)
+        }
+    }}
+
+interface HeatmapTile {
     @GET("heatmapTiles/{type}/{zoom}/{x}/{y}")
     suspend fun getHeatmapTile(
         @Path("type") type: String,
@@ -39,7 +66,6 @@ interface AirQualityApiService {
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
-
             return retrofit.create(AirQualityApiService::class.java)
         }
     }}
