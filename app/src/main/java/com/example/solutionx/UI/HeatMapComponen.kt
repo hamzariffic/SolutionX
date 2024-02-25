7b5d0a5707b7e9dccce05c201c963543b22cf8c3
hamzariffic <hamza.mattea@gmail.com>
2024-02-25 17:08:15 +0300

    heat map component

--- app/src/main/java/com/example/solutionx/UI/HeatMapComponen.kt
+++ app/src/main/java/com/example/solutionx/UI/HeatMapComponen.kt
@@ -1,4 +1,60 @@
 package com.example.solutionx.UI
 
-class HeatMapComponen {
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.asImageBitmap
+import com.example.solutionx.APIService.AirQualityApiService
+import com.example.solutionx.model.HeatmapTileResponse
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+@Composable
+fun HeatmapComponent(
+    type: String,
+    zoom: Int,
+    x: Int,
+    y: Int,
+    airQualityApiService: AirQualityApiService
+) {
+    val heatmapTile = remember { mutableStateOf<Bitmap?>(null) }
+
+    LaunchedEffect(Unit) {
+        val response = getHeatmapTileResponse(type, zoom, x, y, airQualityApiService)
+        if (response != null) {
+            val responseBody = response.getBytes()
+            if (responseBody != null) {
+                BitmapFactory.decodeByteArray(responseBody, 0, responseBody.size)
+                    .also { heatmapTile.value = it }
+            }
+        }
+    }
+
+    heatmapTile.value?.let { image ->
+        Image(
+            bitmap = image.asImageBitmap(),
+            contentDescription = null
+        )
+    }
+}
+
+private suspend fun getHeatmapTileResponse(
+    type: String,
+    zoom: Int,
+    x: Int,
+    y: Int,
+    airQualityApiService: AirQualityApiService
+): HeatmapTileResponse? {
+    return withContext(Dispatchers.IO) {
+        try {
+            airQualityApiService.getHeatmapTile(type, zoom, x, y)
+        } catch (e: Exception) {
+            // Handle exception
+            null
+        }
+    }
 }
