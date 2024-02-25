77e90cef619a2d285e3141e85fcdb1ba604498bc
hamzariffic <hamza.mattea@gmail.com>
2024-02-25 17:06:33 +0300

    history response data model optimized

--- app/src/main/java/com/example/solutionx/model/HistoryResponse.kt
+++ app/src/main/java/com/example/solutionx/model/HistoryResponse.kt
@@ -1,64 +1,19 @@
 package com.example.solutionx.model
 
 import android.os.Parcel
-import android.os.Parcelable
 
 data class HistoryResponse(
     val hoursInfo: List<HourlyInfo>,
     val regionCode: String,
-    val nextPageToken: String? = null
-)
-
-data class HourlyInfo(
+    val nextPageToken: String? = null,
     val dateTime: String,
-    val indexes: List<Index>
+    val indexes: ArrayList<Index>?,
+    val code: Parcel,
+    val displayName: String?,
+    val aqi: Int,
+    val aqiDisplay: String?,
+    val color: Color?,
+    val category: String?,
+    val dominantPollutant: String?
 )
 
-data class Index(
-    val code: Parcel,
-    val displayName: String,
-    val aqi: Int,
-    val aqiDisplay: String,
-    val color: Color,
-    val category: String,
-    val dominantPollutant: String
-) {
-    constructor(code: Parcel) : this(
-    )
-}
-
-data class Color(
-    val red: Int,
-    val green: Int,
-    val blue: Int,
-    val alpha: Int
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readInt(),
-        parcel.readInt(),
-        parcel.readInt(),
-        parcel.readInt()
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeInt(red)
-        parcel.writeInt(green)
-        parcel.writeInt(blue)
-        parcel.writeInt(alpha)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Color> {
-        override fun createFromParcel(parcel: Parcel): Color {
-            return Color()
-        }
-
-        override fun newArray(size: Int): Array<Color?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
