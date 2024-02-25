a4aada9e8a3ebe31c8a1350b43c5c856a1293481
hamzariffic <hamza.mattea@gmail.com>
2024-02-25 17:05:44 +0300

    all bugs fixed

--- app/src/main/java/com/example/solutionx/viewModels/HistoryViewModel.kt
+++ app/src/main/java/com/example/solutionx/viewModels/HistoryViewModel.kt
@@ -1,14 +1,11 @@
 package com.example.solutionx.viewModels
 
 import androidx.lifecycle.ViewModel
-import com.example.solutionx.APIService.AirQualityApiService
 import com.example.solutionx.APIService.AirQualityClient.airQualityHistoryApiService
 import com.example.solutionx.APIService.HourlyInfo
-import com.example.solutionx.model.HistoryLookupRequest
 
 class HistoryViewModel : ViewModel() {
-    private val airQualityApiService = AirQualityApiService.create()
-    suspend fun fetchAirQualityHistory(historyRequest: HistoryLookupRequest): List<HourlyInfo> {
+    suspend fun fetchAirQualityHistory(historyRequest: Unit): List<HourlyInfo> {
         return airQualityHistoryApiService.getAirQualityHistory(historyRequest)
     }
 
