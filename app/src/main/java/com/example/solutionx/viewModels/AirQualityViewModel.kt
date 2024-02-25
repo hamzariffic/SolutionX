eb211d0674a64896df675f3b49740987fb57a991
hamzariffic <hamza.mattea@gmail.com>
2024-02-25 16:16:12 +0300

    added appropriate error handling for the app to ensure it logs errors to the UI

--- app/src/main/java/com/example/solutionx/viewModels/AirQualityViewModel.kt
+++ app/src/main/java/com/example/solutionx/viewModels/AirQualityViewModel.kt
@@ -8,22 +8,30 @@ import com.example.solutionx.model.AirQualityRequest
 import com.example.solutionx.model.AirQualityResponse
 
 class AirQualityViewModel(private val apiService: AirQualityApiService) : ViewModel() {
+
     private val _airQualityData by lazy { MutableLiveData<AirQualityResponse>() }
     val airQualityData: LiveData<AirQualityResponse> = _airQualityData
 
+    private val _errorLiveData by lazy { MutableLiveData<String>() }
+    val errorLiveData: LiveData<String> = _errorLiveData
+
     private var airQualityRequest: AirQualityRequest? = null
 
-    suspend fun fetchAirQualityData(unit: Unit) {
+    suspend fun fetchAirQualityData() {
         airQualityRequest?.let { request ->
             try {
                 val response = apiService.currentConditions(request)
                 if (response.isSuccessful) {
-                    response.body()?.also { it -> _airQualityData.value = it }
+                    response.body()?.also {
+                        _airQualityData.value = it
+                    }
                 } else {
-                    println("Could not get Air Quality Conditions: ${response.message()}")
+                    _errorLiveData.value = "Could not get Air Quality Conditions: ${response.message()}"
+                    _airQualityData.value = null // Clear previous data on error
                 }
             } catch (e: Exception) {
-                println("Network request failed: ${e.message}")
+                _errorLiveData.value = "Network request failed: ${e.message}"
+                _airQualityData.value = null // Clear previous data on error
             }
         }
     }
