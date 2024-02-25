90887a5071fce3dcaa35a01e430b479e79edbc7b
hamzariffic <hamza.mattea@gmail.com>
2024-02-26 02:29:45 +0300

    history viewModel

--- app/src/main/java/com/example/solutionx/viewModels/HistoryViewModel.kt
+++ app/src/main/java/com/example/solutionx/viewModels/HistoryViewModel.kt
@@ -2,10 +2,10 @@ package com.example.solutionx.viewModels
 
 import androidx.lifecycle.ViewModel
 import com.example.solutionx.APIService.AirQualityClient.airQualityHistoryApiService
-import com.example.solutionx.APIService.HistoryLookupRequest
+import com.example.solutionx.APIService.History
 
 class HistoryViewModel : ViewModel() {
-    fun fetchAirQualityHistory(historyRequest: HistoryLookupRequest): List<HistoryLookupRequest> {
-        return airQualityHistoryApiService.getAirQualityHistory(historyRequest)
+    fun fetchAirQualityHistory(historyRequest: com.example.solutionx.model.HistoryLookupRequest) {
+        return airQualityHistoryApiService.getAirQualityHistory(History)
     }
 }
