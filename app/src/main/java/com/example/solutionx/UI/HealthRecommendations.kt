3c3f9fe4522a76600b42b4f0fef777cee284971e
hamzariffic <hamza.mattea@gmail.com>
2024-02-25 20:05:57 +0300

    Health recommendations

--- app/src/main/java/com/example/solutionx/UI/HealthRecommendations.kt
+++ app/src/main/java/com/example/solutionx/UI/HealthRecommendations.kt
@@ -1,4 +1,38 @@
 package com.example.solutionx.UI
 
-class HealthRecommendations {
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.example.solutionx.model.HealthRecommendations
+import com.example.solutionx.viewModels.AirQualityViewModel
+
+@Composable
+fun HealthRecommendationsScreen(
+    navController: NavController,
+    healthRecommendations: HealthRecommendations
+) {
+    // Fetch the air quality data
+    val regionCode = null
+    val request = regionCode?.let { AirQualityRequest(it) }
+    val airQualityResponse by AirQualityViewModel.airQualityData.observeAsState()
+
+    AirQualityViewModel.fetchAirQualityData(request!!)
+    // Display the health recommendations
+    Column {
+        Text("Health Recommendations")
+        Text("General Population: ${healthRecommendations.generalPopulation}")
+        Text("Elderly: ${healthRecommendations.elderly}")
+        Text("Lung Disease Population: ${healthRecommendations.lungDiseasePopulation}")
+        Text("Heart Disease Population: ${healthRecommendations.heartDiseasePopulation}")
+        Text("Athletes: ${healthRecommendations.athletes}")
+        Text("Pregnant Women: ${healthRecommendations.pregnantWomen}")
+        Text("Children: ${healthRecommendations.children}")
+
+        // Button to navigate back to the currentConditions screen
+        Button(onClick = { navController.popBackStack() }) {
+            Text("Back to Current Conditions")
+        }
+    }
 }
