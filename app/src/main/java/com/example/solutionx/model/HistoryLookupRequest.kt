df28ebec8092a1da6991989029a83153772c9f2a
hamzariffic <hamza.mattea@gmail.com>
2024-02-26 00:16:38 +0300

    history lookup request update

--- app/src/main/java/com/example/solutionx/model/HistoryLookupRequest.kt
+++ app/src/main/java/com/example/solutionx/model/HistoryLookupRequest.kt
@@ -1,11 +1,11 @@
 package com.example.solutionx.model
 
-import android.graphics.Color
+import androidx.compose.ui.graphics.Color
 import com.google.android.gms.maps.model.LatLng
 
 data class HistoryLookupRequest(
-    val pageSize: Int,
-    val pageToken: String,
+    val pageSize: Int = 0,
+    val pageToken: String = "",
     val location: LatLng,
     val extraComputations: List<ExtraComputation>,
     val customLocalAqis: List<CustomLocalAqi>,
@@ -13,4 +13,16 @@ data class HistoryLookupRequest(
     val universalAqi: Boolean,
     val languageCode: String,
     val uaqiColor: Color
-)
+) {
+    constructor(pageSize: Int, pageToken: String, location: LatLng) : this(
+        pageSize = pageSize,
+        pageToken = pageToken,
+        location = location,
+        extraComputations = emptyList(),
+        customLocalAqis = emptyList(),
+        timeRange = Unit,
+        universalAqi = false,
+        languageCode = "en",
+        uaqiColor = Color(0xFF00FF00)
+    )
+}
