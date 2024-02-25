dadd4b8024e5d96d2146501f916ac69e97788f74
hamzariffic <hamza.mattea@gmail.com>
2024-02-25 21:15:04 +0300

    added LaunchEffect since the function requires to run in coroutine

--- app/src/main/java/com/example/solutionx/UI/AirQualityDataComponent.kt
+++ app/src/main/java/com/example/solutionx/UI/AirQualityDataComponent.kt
@@ -1,27 +1,28 @@
-@file:Suppress("DEPRECATION", "KotlinConstantConditions", "UNREACHABLE_CODE")
+@file:Suppress("KotlinConstantConditions", "UNREACHABLE_CODE")
 
 package com.example.solutionx.UI
 
-import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.LiveData
-import com.example.solutionx.model.AirQualityRequest
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.solutionx.viewModels.AirQualityViewModel
-import kotlinx.coroutines.CoroutineScope
 
 @Composable
 fun AirQualityDataComponent(viewModel: AirQualityViewModel) {
     val regionCode = null
     val request = regionCode?.let { AirQualityRequest(it) }
     val airQualityResponse by viewModel.airQualityData.observeAsState()
-
-    viewModel.fetchAirQualityData(request!!)
-
+//Had to wrap it here since it's a suspend fun and I can't, presumably, create a coroutine composable
+    LaunchedEffect(Unit) {
+        viewModel.fetchAirQualityData(request!!)
+    }
     airQualityResponse?.let { response ->
         Text(text = response.toString())
     }
@@ -33,18 +34,7 @@ fun AirQualityDataComponent(viewModel: AirQualityViewModel) {
 }
 
 @Composable
-fun AirQualityViewModel.AirQualityDataComponent(request: AirQualityRequest) {
-    val airQualityResponse by airQualityData.observeAsState()
-
-    fetchAirQualityData(request)
-
-    airQualityResponse?.let { response ->
-        Text(text = response.toString())
-    }
-}
-
-@Composable
-private fun <T> LiveData<T>.observeAsState(): MutableState<T?> {
+internal fun <T> LiveData<T>.observeAsState(): MutableState<T?> {
     val state = remember { mutableStateOf<T?>(null) }
     observeForever { state.value = it }
     return state
@@ -54,10 +44,15 @@ fun AirQualityRequest(regionCode: Any) {
     AirQualityRequest(regionCode = regionCode)
 }
 
-private fun CoroutineScope.fetchAirQualityData(viewModel: AirQualityRequest) {
-    fetchAirQualityData(viewModel)
-}
-
 fun AirQualityResponse() {
     AirQualityResponse()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AirQualityDataComponentPreview() {
+    val viewModel: AirQualityViewModel = viewModel()
+    val hourlyHistoryResponse = listOf<com.example.solutionx.model.HourlyInfo>()
+    AirQualityDataComponent(viewModel)
+    HourlyHistoryDataComponent(hourlyHistoryResponse)
 }
