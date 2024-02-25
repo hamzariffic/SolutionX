0041e86d6a017cb85833df147eb90f93ef676b65
hamzariffic <hamza.mattea@gmail.com>
2024-02-26 02:29:17 +0300

    added the information about pollutants

--- app/src/main/java/com/example/solutionx/model/Pollutant.kt
+++ app/src/main/java/com/example/solutionx/model/Pollutant.kt
@@ -6,4 +6,9 @@ data class Pollutant<Concentration>(
     val fullName: String,
     val concentration: Concentration,
     val additionalInfo: PollutantAdditionalInfo
-)
+){
+    data class PollutantAdditionalInfo(
+        val sources: String,
+        val effects: String
+    )
+}
