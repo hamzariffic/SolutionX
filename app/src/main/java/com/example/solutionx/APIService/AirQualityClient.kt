4296e0615137c326d0f36cd9bc6fc067799cbc32
hamzariffic <hamza.mattea@gmail.com>
2024-02-26 18:35:01 +0300

    removed the exposed api key

--- app/src/main/java/com/example/solutionx/APIService/AirQualityClient.kt
+++ app/src/main/java/com/example/solutionx/APIService/AirQualityClient.kt
@@ -3,13 +3,19 @@ package com.example.solutionx.APIService
 import retrofit2.Converter
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.FileInputStream
+import java.util.Properties
 
 object AirQualityClient {
-    private const val API_KEY = "AIzaSyCR-4L-TbWGyM0agrwBsTLWN1HYa7T_2EY"
-
-    private const val BASE_URL = "https://airquality.googleapis.com/v1/currentConditions:lookup?key=$API_KEY"
-    private const val BASE_URL_History = "https://airquality.googleapis.com/v1/history:lookup?key=$API_KEY"
-    private const val BASE_URL_HeatMaps = "https://airquality.googleapis.com/v1/lookup?key=$API_KEY"
+    private val apiKey: String by lazy {
+        val properties = Properties()
+        val localPropertiesFile = FileInputStream("local.properties")
+        properties.load(localPropertiesFile)
+        properties.getProperty("API_KEY") ?: throw IllegalStateException("API_KEY not found in local.properties")
+    }
+    private val BASE_URL = "https://airquality.googleapis.com/v1/currentConditions:lookup?key=$apiKey"
+    private val BASE_URL_History = "https://airquality.googleapis.com/v1/history:lookup?key=$apiKey"
+    private val BASE_URL_HeatMaps = "https://airquality.googleapis.com/v1/lookup?key=$apiKey"
 
 
 //  AirQuality Client
