ad0e5ab8d43a13d64cca9c72320a73ce1e297d31
hamzariffic <hamza.mattea@gmail.com>
2024-02-26 18:35:32 +0300

    extras

--- app/src/main/java/extraScreens.kt
+++ app/src/main/java/extraScreens.kt
@@ -1,4 +1,40 @@
-package com.example.solutionx.UI
-
-class extraScreens {
-}
+//package com.example.solutionx.UI
+//
+//class extraScreens {
+//    @Composable
+//    fun ExtraComputationScreen(extraComputation: ExtraComputation) {
+//        when (extraComputation) {
+//            is ExtraComputation.HEALTH_RECOMMENDATIONS -> HealthRecommendationsScreen()
+//            is ExtraComputation.DOMINANT_POLLUTANT_CONCENTRATION -> DominantPollutantConcentrationScreen()
+//            is ExtraComputation.POLLUTANT_CONCENTRATION -> PollutantConcentrationScreen()
+//            is ExtraComputation.LOCAL_AQI -> LocalAQIScreen()
+//            is ExtraComputation.POLLUTANT_ADDITIONAL_INFO -> PollutantAdditionalInfoScreen()
+//        }
+//    }
+//
+//    @Composable
+//    fun HealthRecommendationsScreen() {
+//        // Display health recommendations
+//    }
+//
+//    @Composable
+//    fun DominantPollutantConcentrationScreen() {
+//        // Display dominant pollutant concentration info
+//    }
+//
+//    @Composable
+//    fun PollutantConcentrationScreen() {
+//        // Display pollutant concentration info
+//    }
+//
+//    @Composable
+//    fun LocalAQIScreen() {
+//        // Display local AQI info
+//    }
+//
+//    @Composable
+//    fun PollutantAdditionalInfoScreen() {
+//        // Display additional pollutant info
+//    }
+//
+//}
