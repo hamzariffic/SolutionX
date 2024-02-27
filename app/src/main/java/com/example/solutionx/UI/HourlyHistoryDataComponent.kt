52a44784a2f84f159a3c365a9ad9b9cb4f7bb3ca
hamzariffic <hamza.mattea@gmail.com>
2024-02-27 20:12:12 +0300

    minor fixes

--- app/src/main/java/com/example/solutionx/UI/HourlyHistoryDataComponent.kt
+++ app/src/main/java/com/example/solutionx/UI/HourlyHistoryDataComponent.kt
@@ -3,7 +3,6 @@ package com.example.solutionx.UI
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.solutionx.model.Color
 
 @Composable
 fun HourlyHistoryDataComponent(hourlyHistoryResponse: List<com.example.solutionx.model.HourlyInfo>) {
@@ -12,6 +11,8 @@ fun HourlyHistoryDataComponent(hourlyHistoryResponse: List<com.example.solutionx
         Text(text = hourlyInfo.toString())
     }
 }
+
+//commented out color for this time since I have a bug to fix
 
 @Preview
 @Composable
@@ -25,7 +26,7 @@ fun PreviewHourlyHistoryDataComponent() {
                     "Universal AQI",
                     73,
                     "73",
-                    Color(118, 202, 51, 255),
+//                    Color(118, 202, 51, 255),
                     "Good air quality",
                     "pm10"
                 )
