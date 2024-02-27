e3c3450866d6506f7f6a88a991130f476a714431
hamzariffic <hamza.mattea@gmail.com>
2024-02-27 20:13:08 +0300

    suppressed and removed this model from usage throughout the app. Some bugs to be fixed...

--- app/src/main/java/com/example/solutionx/model/Color.kt
+++ app/src/main/java/com/example/solutionx/model/Color.kt
@@ -3,30 +3,37 @@ package com.example.solutionx.model
 import android.os.Parcel
 import android.os.Parcelable
 
+//I'm currently not using this file since there are some bugs to deal with and I'm yet to master that from the documentation for the same
+//Will update the codebase with time
+
 data class Color(
     val red: Int,
     val green: Int,
     val blue: Int,
     val alpha: Int?,
 
-    val colorList: List<String> = listOf("#000000", "#009966", "#ffcc00", "#ff9900", "#cc0000", "#660066", "#9933cc")
+//    val colorList: List<String> = listOf("#000000", "#009966", "#ffcc00", "#ff9900", "#cc0000", "#660066", "#9933cc")
 ) : Parcelable {
     constructor(parcel: Parcel) : this(
-        parcel.readInt(),
         parcel.readInt(),
         parcel.readInt(),
         parcel.readValue(Int::class.java.classLoader) as? Int,
         parcel.createStringArrayList()!!
     )
 
-    constructor() : this(0, 0, 0, 0)
+    constructor(
+        readInt: Int,
+        readInt1: Int,
+        i3: Int?,
+        createStringArrayList: ArrayList<String>
+    ) : this(0, 0, 0, 0)
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeInt(red)
         parcel.writeInt(green)
         parcel.writeInt(blue)
         parcel.writeValue(alpha)
-        parcel.writeStringList(colorList)
+        parcel.writeStringList()
     }
 
     override fun describeContents(): Int {
@@ -43,6 +50,10 @@ data class Color(
         }
     }
 
+}
+
+private fun Parcel.writeStringList() {
+    TODO("Not yet implemented")
 }
 
 //data class Color(
