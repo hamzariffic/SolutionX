3305eb37bcb86ac220b10878948727c0406bd5fc
hamzariffic <hamza.mattea@gmail.com>
2024-02-27 11:40:31 +0300

    home updates

--- app/src/main/java/com/example/solutionx/UI/Home.kt
+++ app/src/main/java/com/example/solutionx/UI/Home.kt
@@ -10,16 +10,10 @@ import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Observer
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.solutionx.ui.theme.SolutionXTheme
@@ -55,21 +49,6 @@ fun Home(navController: NavController) {
     }
 }
 
-
-@Composable
-fun <T> LiveData<T>.observeAsState(initialValue: T): State<T> {
-    val state = remember { mutableStateOf(initialValue) }
-    val observer = Observer<T> { value ->
-        state.value = value
-    }
-    observeForever(observer)
-    DisposableEffect(this) {
-        onDispose {
-            removeObserver(observer)
-        }
-    }
-    return state
-}
 
 @Preview()
 @Composable
