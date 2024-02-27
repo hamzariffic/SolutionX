0bf333654849f234214ece2747dfcc38438792c3
hamzariffic <hamza.mattea@gmail.com>
2024-02-27 20:16:27 +0300

    history response view model configured to be compatible with the observer

--- app/src/main/java/com/example/solutionx/viewModels/HistoryViewModel.kt
+++ app/src/main/java/com/example/solutionx/viewModels/HistoryViewModel.kt
@@ -1,11 +1,11 @@
 package com.example.solutionx.viewModels
 
 import androidx.lifecycle.ViewModel
-import com.example.solutionx.APIService.AirQualityClient.airQualityHistoryApiService
-import com.example.solutionx.APIService.History
+import com.example.solutionx.model.HistoryResponse
 
 class HistoryViewModel : ViewModel() {
-    fun fetchAirQualityHistory(historyRequest: com.example.solutionx.model.HistoryLookupRequest) {
-        return airQualityHistoryApiService.getAirQualityHistory(History)
+    fun fetchAirQualityHistory(historyRequest: com.example.solutionx.model.HistoryLookupRequest): HistoryResponse {
+        return HistoryResponse(emptyList(), "", null, "", null, null, "", 0, null, null, null, null)
+
     }
 }
