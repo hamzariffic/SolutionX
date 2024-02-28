c9368a2e2bea92f851848ed1953badeaa069da65
hamzariffic <hamza.mattea@gmail.com>
2024-02-28 12:12:37 +0300

    modified data types of some return functions

--- app/src/main/java/com/example/solutionx/UI/HourlyHistoryDataComponent.kt
+++ app/src/main/java/com/example/solutionx/UI/HourlyHistoryDataComponent.kt
@@ -1,37 +1,24 @@
 package com.example.solutionx.UI
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.solutionx.model.ExtraComputation
 
 @Composable
-fun HourlyHistoryDataComponent(hourlyHistoryResponse: List<com.example.solutionx.model.HourlyInfo>) {
-    // Display the list of HourlyInfo data in an area
-    for (hourlyInfo in hourlyHistoryResponse) {
-        Text(text = hourlyInfo.toString())
+fun ExtraComputationListComponent(extraComputations: List<ExtraComputation>) {
+    LazyColumn {
+        items(extraComputations) { extraComputation ->
+            Text(text = extraComputation.toString())
+        }
     }
 }
 
-//commented out color for this time since I have a bug to fix
-
 @Preview
 @Composable
-fun PreviewHourlyHistoryDataComponent() {
-    // Passing a sample HourlyInfo data for preview
-    val sampleHourlyInfo = listOf(
-        com.example.solutionx.model.HourlyInfo(
-            "2024-02-24T15:00:00Z", listOf(
-                com.example.solutionx.model.Index(
-                    "uaqi",
-                    "Universal AQI",
-                    73,
-                    "73",
-//                    Color(118, 202, 51, 255),
-                    "Good air quality",
-                    "pm10"
-                )
-            )
-        )
-    )
-    HourlyHistoryDataComponent(sampleHourlyInfo)
+fun PreviewExtraComputationListComponent() {
+    val extraComputationList = ExtraComputation.entries
+    ExtraComputationListComponent(extraComputationList)
 }
--- app/src/main/java/com/example/solutionx/model/airQualityRequest.kt
+++ app/src/main/java/com/example/solutionx/model/airQualityRequest.kt
@@ -1,20 +1,20 @@
-package com.example.solutionx.model
+    package com.example.solutionx.model
 
-import com.google.type.LatLng
+    import com.google.type.LatLng
 
-data class AirQualityRequest(
-    val location: LatLng,
-    val extraComputations: List<ExtraComputation>,
-    val customLocalAqis: List<CustomLocalAqi>,
-    val universalAqi: Boolean,
-    val languageCode: String
-) {
-    constructor(regionCode: Any) : this(
-        location = regionCode as LatLng,
-        extraComputations = emptyList(),
-        customLocalAqis = emptyList(),
-        universalAqi = false,
-        languageCode = "en"
-    )
+    data class AirQualityRequest(
+        val location: LatLng,
+        val extraComputations: List<ExtraComputation>,
+        val customLocalAqis: List<CustomLocalAqi>,
+        val universalAqi: Boolean,
+        val languageCode: String
+    ) {
+        constructor(regionCode: Any) : this(
+            location = regionCode as LatLng,
+            extraComputations = emptyList(),
+            customLocalAqis = emptyList(),
+            universalAqi = false,
+            languageCode = "en"
+        )
 
-}
+    }
