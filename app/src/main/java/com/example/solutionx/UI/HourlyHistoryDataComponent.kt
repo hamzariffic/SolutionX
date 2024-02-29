349e43cdaa9d89e93cb2bb5fc089244f87191c3b
hamzariffic <hamza.mattea@gmail.com>
2024-02-29 22:50:08 +0300

    More code cleanup

--- app/src/main/java/com/example/solutionx/UI/HourlyHistoryDataComponent.kt
+++ app/src/main/java/com/example/solutionx/UI/HourlyHistoryDataComponent.kt
@@ -1,24 +1,38 @@
 package com.example.solutionx.UI
 
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.solutionx.model.ExtraComputation
+import com.example.solutionx.model.HourlyInfo
 
 @Composable
-fun ExtraComputationListComponent(extraComputations: List<ExtraComputation>) {
-    LazyColumn {
-        items(extraComputations) { extraComputation ->
-            Text(text = extraComputation.toString())
-        }
+fun HourlyHistoryDataComponent(HourlyInfo: List<HourlyInfo>) {
+    // Display the list of HourlyInfo data in an area
+    for (hourlyInfo in HourlyInfo) {
+        Text(text = hourlyInfo.toString())
     }
 }
 
+//commented out color for this time since I have a bug to fix
+
 @Preview
 @Composable
-fun PreviewExtraComputationListComponent() {
-    val extraComputationList = ExtraComputation.entries
-    ExtraComputationListComponent(extraComputationList)
+fun PreviewHourlyHistoryDataComponent() {
+    // Passing a sample HourlyInfo data for preview
+    val sampleHourlyInfo = listOf(
+        com.example.solutionx.model.HourlyInfo(
+            "2024-02-24T15:00:00Z", listOf(
+                com.example.solutionx.model.Index(
+                    "uaqi",
+                    "Universal AQI",
+                    73,
+                    "73",
+//                    Color(118, 202, 51, 255),
+                    "Good air quality",
+                    "pm10"
+                )
+            )
+        )
+    )
+    HourlyHistoryDataComponent(sampleHourlyInfo)
 }
