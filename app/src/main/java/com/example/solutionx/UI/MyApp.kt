da857cc809fb3f9abf0bb0b8d6b31104c3438aaa
hamzariffic <hamza.mattea@gmail.com>
2024-03-09 18:08:36 +0300

    Removed unnecesarry composables

--- app/src/main/java/com/example/solutionx/UI/MyApp.kt
+++ app/src/main/java/com/example/solutionx/UI/MyApp.kt
@@ -1,24 +1,15 @@
 package com.example.solutionx.UI
 
 import androidx.compose.runtime.Composable
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.solutionx.APIService.AirQualityApiService
-import com.example.solutionx.model.HealthRecommendations
 
 @Composable
 fun MyApp(navController: NavHostController) {
     NavHost(navController = navController, startDestination = "Home") {
         composable("Home") { Home(navController) }
-        composable("airQualityDataComponent") {
-            AirQualityDataComponent(viewModel())
-            HourlyHistoryDataComponent(hourlyHistoryResponse = listOf())
-        }
-        composable("HourlyHistoryDataComponent") {
-            HourlyHistoryDataComponent(hourlyHistoryResponse = listOf())
-        }
         composable("HeatMapData/{type}/{zoom}/{x}/{y}") { backStackEntry ->
             val type = backStackEntry.arguments?.getString("type") ?: ""
             val zoom = backStackEntry.arguments?.getInt("zoom") ?: 0
@@ -28,12 +19,6 @@ fun MyApp(navController: NavHostController) {
         }
         composable("HistoryScreen"){
             HistoryScreen(navController)
-        }
-        composable("HealthRecommendationsScreen"){
-            HealthRecommendationsScreen(
-                navController,
-                healthRecommendations = HealthRecommendations()
-            )
         }
         composable("HeatmapScreen"){
             HeatmapScreen()
