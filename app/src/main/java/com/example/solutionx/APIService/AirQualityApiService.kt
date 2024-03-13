0d34d42fd70bfe0c7593c4e4a299b55679a9ecf6
hamzariffic <hamza.mattea@gmail.com>
2024-03-14 01:06:12 +0300

    removed unnecessary dependencies

--- app/src/main/java/com/example/solutionx/APIService/AirQualityApiService.kt
+++ app/src/main/java/com/example/solutionx/APIService/AirQualityApiService.kt
@@ -1,5 +1,6 @@
 package com.example.solutionx.APIService
 
+import com.example.solutionx.BuildConfig.API_KEY
 import com.example.solutionx.model.AirQualityResponse
 import com.example.solutionx.model.HeatmapTileResponse
 import retrofit2.Retrofit
@@ -29,6 +30,11 @@ interface AirQualityApiService {
         @Path("y") y: Int
     ): HeatmapTileResponse
 
+    suspend fun getHistory(key: String, location: String) {
+
+            getAirQualityHistory(HistoryLookupRequest(key = API_KEY, pageToken = location))
+    }
+
     companion object {
         private const val BASE_URL = "https://airquality.googleapis.com/v1/"
 
@@ -43,46 +49,3 @@ interface AirQualityApiService {
 }
 
 
-interface History {
-    @POST("history?:lookup=API_KEY")
-    suspend fun getAirQualityHistory(
-        @Body historyLookupRequest: Unit
-    ): List<HistoryLookupRequest>
-
-    companion object {
-        private const val BASE_URL_HISTORY = "https://airquality.googleapis.com/v1/"
-
-        fun create(): History {
-            val retrofit = Retrofit.Builder()
-                .baseUrl(BASE_URL_HISTORY)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-            return retrofit.create(History::class.java)
-        }
-    }
-}
-
-
-interface HeatmapTile {
-    val data: Any
-
-    @GET("heatmapTiles?/{type}/{zoom}/{x}/{y}")
-    suspend fun getHeatmapTile(
-        @Path("type") type: String,
-        @Path("zoom") zoom: Int,
-        @Path("x") x: Int,
-        @Path("y") y: Int
-    ): HeatmapTileResponse
-
-    companion object {
-        private const val BASE_URL_HEATMAP = "https://airquality.googleapis.com/v1/"
-
-        fun create(): HeatmapTile {
-            val retrofit = Retrofit.Builder()
-                .baseUrl(BASE_URL_HEATMAP)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-            return retrofit.create(HeatmapTile::class.java)
-        }
-    }
-}
