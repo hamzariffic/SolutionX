0cc2709cd84788daaa06ab8c033092dfbbeb2583
hamzariffic <hamza.mattea@gmail.com>
2024-03-16 14:34:50 +0300

    History screen edits implementation

--- app/src/main/java/com/example/solutionx/UI/HistorysScreen.kt
+++ app/src/main/java/com/example/solutionx/UI/HistorysScreen.kt
@@ -2,7 +2,6 @@
 
 package com.example.solutionx.UI
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
