fe29c2fb1a69661f10f3d682eea0be9d7c8f02ff
hamzariffic <hamza.mattea@gmail.com>
2024-03-26 07:36:53 +0300

    adding the toggle functionality to allow for map expansion

--- app/src/main/java/com/example/solutionx/UI/HeatMapScreen.kt
+++ app/src/main/java/com/example/solutionx/UI/HeatMapScreen.kt
@@ -1,30 +1,65 @@
-package com.example.solutionx.UI
-
+import android.graphics.Bitmap
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.solutionx.APIService.AirQualityApiService
-import com.example.solutionx.model.MapType
+import com.example.solutionx.model.HeatmapTileResponse
+import com.example.solutionx.model.MapType.US_AQI
 
 @Composable
-fun HeatmapScreen() {
-    var showHeatmap by remember { mutableStateOf(false) }
+fun HeatmapScreen(airQualityApiService: Nothing?) {
+    val heatmapTile = remember { mutableStateOf<Bitmap?>(null) }
 
     Column(modifier = Modifier.fillMaxSize()) {
-        Button(onClick = { showHeatmap = !showHeatmap }) {
-            Text(if (showHeatmap) "Hide Heatmap" else "Show Heatmap")
+        Button(onClick = { fetchHeatmapData(
+            airQualityApiService = airQualityApiService,
+            heatmapTile = heatmapTile
+        ) }) {
+            Text("Toggle Heatmap")
         }
 
-        if (showHeatmap) {
-            HeatmapComponent(type = MapType.US_AQI.toString(), zoom = 2, x = 0, y = 1, AirQualityApiService.create())
+        heatmapTile.value?.let {
+            HeatmapComponent(it)
+        }
+    }
+}
+
+@Composable
+private fun HeatmapComponent(heatmapBitmap: Bitmap) {
+    Image(
+        bitmap = ImageBitmap(heatmapBitmap),
+        contentDescription = null,
+        modifier = Modifier.fillMaxSize(),
+        contentScale = ContentScale.FillBounds
+    )
+}
+
+@Composable
+private fun fetchHeatmapData(
+    airQualityApiService: Nothing?,
+    heatmapTile: MutableState<Bitmap?>
+) {
+    LaunchedEffect(Unit) {
+        val response = airQualityApiService?.let {
+            HeatmapTileResponse(
+                US_AQI.toString(), 2, listOf(0), 1.toString(),
+                it
+            )
+        }
+        response?.let {
+            if (it.isSuccessful) {
+                heatmapTile.value = it.data
+            }
         }
     }
 }
@@ -32,5 +67,6 @@ fun HeatmapScreen() {
 @Preview
 @Composable
 fun HeatMapScreen() {
-    HeatmapScreen()
+    val airQualityApiService = null
+    HeatmapScreen(airQualityApiService)
 }
